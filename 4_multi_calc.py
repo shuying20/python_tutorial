7a71438eec18f27dd6a6f3d500130752b12584f5
aurel <calista.aurellie@gmail.com>
2023-03-20 10:12:04 +0800

    idk

--- 4_multi_calc.py
+++ 4_multi_calc.py
@@ -1,6 +1,7 @@
 # 1. Input
 x1= input("enter x1: ")
 x2= input("enter x2: ")
+x3= input("enter x3: ")
 op = input("enter operator: ")
 
 #2. Process
@@ -13,17 +14,20 @@ if op == "*":
 if op == "/":
     sum= int(x1) / int(x2)
 elif op == "avg":
-    sum= (int(x1) + int(x2))/2
-elif op == "sd":
-    sum= (int(x1) + int(x2))/2
+    sum= (int(x1) + int(x2) + int(x3))/2
+
 
 #HW
-# CALC MEAN
+# CALC MEAN x1=5, x2=10, x3=11
 1. tmp1 = mean - x1
 2. tmp2 = mean - x2
-3. (tmp1+tmp2)**2
-4.#3/m
-5. sqrt (#4)
+3. tmp3 = mean - x3
+4. (tmp1+tmp2)**2
+5.#3/m
+6. sqrt (#4)
+import math
+elif op == "sd":
+    sd= (int(avg-x1)) + int(avg-x2) + int(avg-x3)**2)
 
 #3. output
 print (f"sum: {sum}")
---
+++ 5_loop.py
@@ -0,0 +1,17 @@
+# 1. Input
+sum=0
+#loop HAVE to put indentation to show its a part of the loop
+while True:
+    #loop while "true"=continous, if specific do while "..."
+    x1= input("enter x1: ")
+    x2= input("enter x2: ")
+    op = input("enter operator: ")
+
+    #2. Process
+    if op == "+":
+        sum= int(x1) + int(x2)
+    if op == "-":
+        sum= int(x1) - int(x2)
+
+    #3. output
+    print (f"sum: {sum}")
