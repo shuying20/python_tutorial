82371f16cc08a9735241811934e430ebf5dd9b5b
aurel <calista.aurellie@gmail.com>
2023-03-31 14:52:45 +0800

    homework 5

--- 10_DF_sort.py
+++ 10_DF_sort.py
@@ -1,18 +1,9 @@
-#
-# Template for Python program
-# Name: James Bond
-# Date : 2023/03/27
-#
-
 import pandas as pd
 
-# 1. Input
 raw_data = pd.read_csv("Menu.csv")
 print(raw_data)
 print(raw_data.info())
-
-# 2. Process
-raw_data.sort_values(["Menu"],axis=0,inplace=True)
-
-# 3. Output
-print(raw_data)
+numberOfItems = len(raw_data)
+sorted_data = raw_data.sort_values("Menu",ascending=True)
+print(f'#1 -> Count:{numberOfItems}')
+print(f'#2 {sorted_data}')
