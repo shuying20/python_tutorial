1da69f0d69caf198d855e7b2042b2f2d8fba0b18
aurel <calista.aurellie@gmail.com>
2023-04-17 09:13:46 +0800

    ,

---
+++ 12_function_1.py
@@ -0,0 +1,37 @@
+import statistics, math
+
+def my_add (input):
+    sum = 0
+    for x in input:
+        sum += int(x)
+    return sum
+
+input = [10,20,30]
+
+#2. process
+answer = my_add(input)
+
+#3. output
+print(f'Answer: {answer}')
+
+#MULTIPLY
+def my_add (input):
+    sum = 1
+    for x in input:
+        sum *= int(x)
+    return sum
+
+input = [20,40,600]
+answer = my_add(input)
+print(f'Answer: {answer}')
+
+#STAND DEV
+def my_add (input):
+    sum = 1
+    for x in input:
+        sum *= int(x)
+    return sum
+
+input = [20,40,600]
+answer = my_add(input)
+print(f'Answer: {answer}')
